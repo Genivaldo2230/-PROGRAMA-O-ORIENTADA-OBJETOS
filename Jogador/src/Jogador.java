4f2232283c6d080da1ae2e8027c4ccae35d56578
GDA-NOT-DELL <gda_anjos@hotmail.com.br>
2023-02-24 21:32:07 -0300

    Projeto jogo Version 1.0 criação do metodo e get e set

---
+++ Jogador/src/Jogador.java
@@ -0,0 +1,54 @@
+public class Jogador {
+
+    private String nome;
+    
+    private int xp = 0;
+    private int hp = 100;
+    private boolean envenenado = true;
+
+    public  Jogador(){
+
+    }
+
+    public Jogador(String nome){
+        this.nome = nome;
+    }
+
+    public void receberDanos(int pontos){
+        this.hp -= pontos;
+    }
+    
+    public void receberCura(int pontos){
+        this.xp += pontos;
+    }
+    public void receberExperiencia(int pontos){
+        this.xp += pontos;
+    }
+    
+    public void receberAntidoto(){
+     envenenado = !envenenado;         
+    }
+    
+    public int getXp() {
+        return this.xp;
+    }
+    public int getHp() {
+        return this.hp;
+    }
+    
+    public boolean getEnvenenado() {
+        return envenenado;
+    }
+    public void setEnvenenado(boolean envenenado) {
+        this.envenenado = envenenado;
+    }
+    
+    public boolean isEnvenenado(){
+        return this.envenenado;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+}
